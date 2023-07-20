ae65e81578c10cd0dc70e382d11b552d8a480b70
João Vitor Alves Ribeiro <joao.ribeiro@phb.com>
2023-07-20 16:40:26 -0300

    projeto portal educacional inicioado exe1

---
+++ OrientacaoObjeto/Aulas/exe1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace exe1
+{
+        class Program
+    {
+        Boolean ap = true;
+        static void Main(String[] args)
+        {
+            Aluno obj1 = new Aluno();
+            Console.WriteLine(" bem vindo!! \n Voce é Aluno ou Professor ?");
+            
+            string leituraI = Console.ReadLine();
+            if(leituraI == "aluno"){
+                Console.WriteLine("Voce ja possui cadastro ou email Educacional?!! \n Sim ou Não");
+                string alunoQ = Console.ReadLine();
+                if(alunoQ == "sim"){
+                    Console.WriteLine("OLÁ Aluno \n Digite seu email educacional e sua senha (Lembrando que a senha é seu RA) ");
+                }
+                else{
+                    Console.WriteLine("Digite seu email pessoal");
+                    string cadastroA1 = Console.ReadLine();
+                    Console.WriteLine("Digite seu numero de telefone");
+                    int cadastroA2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Digite seu nome");
+                    string cadastroA3 = Console.ReadLine();
+                    obj1.cadastro(cadastroA3, cadastroA1, cadastroA2);
+                }
+            }else{
+                Console.WriteLine("OlÁ novamente \n Digite seu cpf e senha para entrar no portal dos professores");
+            }
+
+        }
+    }
+}
---
+++ OrientacaoObjeto/Aulas/exe2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exe2
+{
+
+    class Program
+    {
+        static void Main(String[] args)
+        {}
+
+        }
+    }
